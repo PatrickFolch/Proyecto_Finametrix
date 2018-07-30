2b45d15408edf347b92c486737b104413dafbc73
PatrickFolch <patrickfolch2@gmail.com>
2018-07-30 15:10:43 +0200

    errores de calculo solucionados

--- services/calcRentService.js
+++ services/calcRentService.js
@@ -1,40 +1,54 @@
-class CalcRentService{
-    constructor(data,isin){
-        this.jsonObject=data;
+class CalcRentService {
+    constructor(data, isin) {
+        this.jsonObject = data;
 
     }
-    calcRent(finicio, ffin){
-            
+    calcRent(finicio, ffin) {
+
         let lineas = JSON.parse(JSON.stringify(this.jsonObject));
-                let rentavilidad=0.0;
-               let precioinicio = 0;
-               let preciofin = 0;
-               let number = 0 
-                for (let i=0; i<lineas.length; i++)
-                { number = parseFloat(lineas[i].field4)
-                    if (lineas[i].field3==finicio){
-                        precioinicio +=number
-                    }
-                    if (lineas[i].field3==ffin){
-                        preciofin +=number
-                    }
-                }
-         
-                
-                precioinicio= lineas[0].field4;
-                preciofin=number
-                //console.log('precio fin '+PrecioFin);
-                
-                //console.log('precio inicio '+PrecioInicio);
-                console.log(preciofin)
-                console.log(precioinicio)
-                 rentavilidad = (preciofin - precioinicio)/precioinicio
-                 console.log('esta es la rentavilidad '+rentavilidad)
-                 return rentavilidad;
-                console.log('total '+(preciofin - precioinicio)/precioinicio);
-                
-                
-        };
+        let rentabilidad = 0;
+        let precioinicio = 0;
+        let preciofin = 0;
+    
+        //    console.log(lineas) 
+        lineas.forEach(linea => {
+            if (linea.field3 >= finicio && linea.field3 <= ffin) {
+                console.log(parseFloat(linea.field4.replace(',' , '.')));
+                preciofin += parseFloat(linea.field4.replace(',' , '.'))
+                   //console.log(linea);
+                   
+            }
+          
+        });
+        //    for (let i=0; i<lineas.length; i++)
+        //     { 
+        //         number = parseFloat(lineas[i].field4)
+        //         if (lineas[i].field3>=finicio && lineas[i].field3<=ffin){
+        //             preciofin +=number
+        //             console.log(number);
+        //         }
+        //         //if (lineas[i].field3==ffin){
+        //             //preciofin +=number
+        //         //}
+        //     }
+
+
+        precioinicio = lineas[0].field4;
+        //console.log(lineas);
+
+
+        //console.log('precio fin '+PrecioFin);
+
+        //console.log('precio inicio '+PrecioInicio);
+        console.log('precio fin ' + preciofin)
+        console.log('precio inicio ' + precioinicio)
+        rentabilidad = (preciofin - precioinicio) / precioinicio
+        console.log('esta es la rentabilidad ' + rentabilidad)
+        return rentabilidad;
+        console.log('total ' + (preciofin - precioinicio) / precioinicio);
+
+
+    };
 };
 
-module.exports=CalcRentService;
+module.exports = CalcRentService;
--- services/calcVolService.js
+++ services/calcVolService.js
@@ -1,35 +1,40 @@
-class CalcVolService{
-    constructor(data){
+class CalcVolService {
+    constructor(data,isin) {
         this.jsonObject = data;
-        
+
         //console.log('Values'+this.jsonObject);
     }
-    calcVol(finicio, ffin){
-        
+    calcVol(finicio, ffin) {
+
         
         let lineas = JSON.parse(JSON.stringify(this.jsonObject));
-        let values =[];
-        
-        for (let i=0; i<lineas.length; i++)
-        {
-            values.push(parseFloat(lineas[i].field4.replace(',' , '.')));
-             
+        let values = [];
+
+        for (let i = 0; i < lineas.length; i++) {
+            if (lineas[i].field3>=finicio && lineas[i].field3<=ffin){
+            values.push(parseFloat(lineas[i].field4.replace(',', '.')));
+            }
         }
-       
+
         // console.log(values);
-            
-            let sum = values.reduce((previous, current) => current += previous);
-            let media = parseFloat( sum / values.length);
-            // console.log('Media '+media);
-            let varianza = values.reduce((total, current)=>{
-         
-                 return Math.pow((parseFloat(current)- media),2)});
-            //let varianza = Math.pow(media - parseFloat(lineas[i].field4),2);                                                         
-            // console.log('Varianza ->'+ varianza);
-            let varianzaTotal=varianza += varianza/values.length;
-            let volatilidad= Math.sqrt(varianzaTotal);
-            return volatilidad;
+
+        let sum = values.reduce((previous, current) => current += previous);
+        let media = parseFloat(sum / values.length);
+        // console.log('Media '+media);
+
+        let varianza = values.reduce((total, current) => {
+
+            return Math.pow((parseFloat(current) - media), 2)
+        });
+        //let varianza = Math.pow(media - parseFloat(lineas[i].field4),2);                                                         
+        console.log('Varianza ->'+ varianza);
+        let varianzaTotal = varianza / (values.length - 1);
+        console.log('VT '+varianzaTotal);
+        let volatilidad = Math.sqrt(varianzaTotal);
+        console.log('VOLATILIDAD->'+volatilidad);
+        return volatilidad;
+        
     }
 }
 
-module.exports=CalcVolService;
+module.exports = CalcVolService;
